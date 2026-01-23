0bfbbf72d9b36e6a5062cf6a838537ac3c7106a0
Jenni Korvenpaa <jkorvenp@c1r2p6.hive.fi>
2026-01-23 14:52:36 +0200

    feat: ADD requires input for all fields + messages

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,50 +1,78 @@
 #include "PhoneBook.hpp"
 
 PhoneBook::PhoneBook(){
-    _index = -1;
+	_index = -1;
 }
 PhoneBook::~PhoneBook(){
 
 }
 
 int PhoneBook::get_index(){
-    if (_index > 7)
-        _index = 0;
-    else
-        _index++;
-    return _index;
+	if (_index >= 7)
+		_index = 0;
+	else
+		_index++;
+	return _index;
 }
 
 void   PhoneBook::add_contact(){
-    std::string input;
+	std::string input;
 
-    std::cout << "first Name: ";
-    std::cin >> input;
-    _contact[_index].Contact::set_firstname(input);
+	while (input.empty()){
+		std::cout << "first name: ";
+		std::getline(std::cin, input);
+	}
+	_contact[_index].Contact::set_firstname(input);
+	while (1){
+		std::cout << "last name: ";
+		std::getline(std::cin, input);
+		if (!input.empty())
+			break;
+	}
+	_contact[_index].Contact::set_lastname(input); 
 
+	while (1){
+		std::cout << "nickname: ";
+		std::getline(std::cin, input);
+		if (!input.empty())
+			break;
+	}
+	_contact[_index].Contact::set_nickname(input);
 
-    std::cout << "last name: ";
-    std::cin >> input;
-    _contact[_index].Contact::set_lastname(input); 
+	while (1){
+		std::cout << "phone number: ";
+		std::getline(std::cin, input);
+		if (!input.empty())
+			break;
+	}
+	_contact[_index].Contact::set_phone(input); 
 
-
-    std::cout << "nickname: ";
-    std::cin >> input;
-    _contact[_index].Contact::set_nickname(input); 
-
-
-    std::cout << "phone number: ";
-    std::cin >> input;
-    _contact[_index].Contact::set_phone(input); 
-
-    std::cout << "Darkest secret: ";
-    std::getline(std::cin, input);
-    _contact[_index].Contact::set_secret(input);
+	while (1){
+		std::cout << "Darkest secret: ";
+		std::getline(std::cin, input);
+		if (!input.empty())
+			break;
+	}
+	_contact[_index].Contact::set_secret(input);
+	std::cout << "Added contact " << _index << " to PhoneBook" << std::endl;  
 }
 
 void   PhoneBook::search_contact(){
 
-    int pos = 0; // std::cin
-    std::cout << pos << "|";
-    std::cout << _contact[pos].Contact::get_firstname() << "|" << std::endl;
+	int pos = 0; // std::cin
+	std::cout << pos << "|";
+	std::cout << _contact[pos].Contact::get_firstname() << "|" << std::endl;
+	std::cout << _contact[pos].get_secret() << std::endl;
 }
+/*
+setw(10);
+std::cin fhesukhfkueshaku    \n
+input fhesukhfkueshaku
+std::cin     \n
+ignore
+std::cin hdhakff fehsfuksehaklu hfueskhakfuhsel\n
+ // std::cin.ignore();
+
+
+
+*/
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -5,10 +5,15 @@ int	main(){
     std::string input;
     PhoneBook book;
 
+    std::cout << "PhoneBook opened: type ADD, SEARCH or EXIT" << std::endl; 
     while (1){
-        std::cin >> input;
-        if (input == "EXIT")
+        
+        if (!std::getline(std::cin , input))
+            break; 
+        if (input == "EXIT"){
+            std::cout << "Closing PhoneBook" << std::endl; 
             break;
+        }
         else if (input == "ADD"){
             book.get_index();
             book.add_contact();
