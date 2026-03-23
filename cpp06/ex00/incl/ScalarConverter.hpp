f5048c5b3b8ad131c5950ae8811d961c05f833c7
Jenni Korvenpaa <jkorvenp@c1r6p17.hive.fi>
2026-03-23 17:14:05 +0200

    feat:ex00 type logic (incomplete)

--- cpp06/ex00/incl/ScalarConverter.hpp
+++ cpp06/ex00/incl/ScalarConverter.hpp
@@ -1,15 +1,28 @@
 #pragma once
 
 #include <iostream>
+// #include <cstring>
+// #include <cctype>
 
 class ScalarConverter
 {
+private:
+	enum Type{
+		 CHAR,
+		 INT,
+		 FLOAT,
+		 DOUBLE,
+		 INVALID,
+	};
+	static Type _type;
+	
 public:
 	ScalarConverter() = delete;
 	~ScalarConverter() = delete;
 	ScalarConverter(const ScalarConverter &other) = delete;
 	ScalarConverter	&operator=(const ScalarConverter &other) = delete;
 	
+	static void setType(std::string literal);
 	static void convert(std::string literal);
 };
 
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -1,7 +1,86 @@
 #include "ScalarConverter.hpp"
 
-void ScalarConverter::convert(std::string literal){
+static void	intLiteral(std::string literal){
+	std::cout << literal << std::endl;
 
+}
+static void	floatLiteral(std::string literal){
 	std::cout << literal << std::endl;
 }
+
+static void	doubleLiteral(std::string literal){
+	std::cout << literal << std::endl;
+}
+
+static void	charLiteral(std::string literal){
+	std::cout << literal << std::endl;
+}
+
+static void	invalid(std::string literal){
+	std::cout << literal << std::endl;
+}
+
+void ScalarConverter::setType(std::string literal){
+
+	int i = 0;
+	
+	if ((int)literal.length() == 1){
+		if (std::isdigit(literal[i]))
+			_type = INT;
+		else if (std::isprint(literal[i]))
+			_type = CHAR;
+		else
+			_type = INVALID;
+		return ;
+	}
+
+	int dot = 0;
+	for (int i = 0; i < (int)literal.length(); i++){
+		if (!std::isdigit(literal[i])){
+			if (literal[i] == '.'){
+				dot++;
+				continue;
+			}
+			if (literal[i] == 'f' && i == (int)literal.length() - 1 && dot <= 1){
+				_type = FLOAT;
+				return ;
+			}
+			else{
+				_type = INVALID;
+				return ;
+			}
+		}
+	}
+	if (dot == 1)
+		_type = DOUBLE;
+	else if (dot == 0)
+		_type = INT;
+	else
+		_type = INVALID;
+}
+
+
+void ScalarConverter::convert(std::string literal){
+
+	setType(literal);
+	switch(_type)
+	{
+		case 0:
+			charLiteral(literal);
+			break ;
+		case 1:
+			intLiteral(literal);
+			break ;
+		case 2:
+			floatLiteral(literal);
+			break ;
+		case 3:
+			doubleLiteral(literal);
+			break ;
+		default:
+			invalid(literal);
+	}
+	
+}
+
 
