01df05cf319f2b216e129a6096bb5751e17bc3b4
Jenni Korvenpaa <jkorvenp@c1r5p12.hive.fi>
2026-03-28 17:20:16 +0200

    feat ex02

--- cpp06/ex02/incl/Base.hpp
+++ cpp06/ex02/incl/Base.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <random>
+#include <iostream>
 
 class Base
 {
---
+++ cpp06/ex02/src/main.cpp
@@ -0,0 +1,60 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+
+Base * generate(void){
+
+	std::uniform_int_distribution<> dist(1, 3);
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	int x = dist(gen);
+	if (x == 1)
+		return new A;
+	else if (x == 2)
+		return new B;
+	else
+		return new C;
+}
+
+void identify(Base* p){
+	if (dynamic_cast<A*>(p))
+		std::cout << "type A" << std::endl;
+	if (dynamic_cast<B*>(p))
+		std::cout << "type B" << std::endl;
+	if (dynamic_cast<C*>(p))
+		std::cout << "type C" << std::endl;
+}
+
+void identify(Base& p){
+	try{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "Type A" << std::endl;
+	}
+	catch(std::bad_cast &e){
+
+	}
+	try{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "Type B" << std::endl;
+	}
+	catch(std::bad_cast &e){
+
+	}
+	try{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "Type C" << std::endl;
+	}
+	catch(std::bad_cast &e){
+
+	}
+}
+
+int main(void){
+	Base *p = generate();
+	identify(p);
+	identify(*p);
+	delete p;
+	return (0);
+}
