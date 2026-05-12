4b698f7cc601c091b7ae7a98d088084238ed0089
Jenni Korvenpaa <jkorvenp@c1r5p16.hive.fi>
2026-05-12 15:34:02 +0300

    fix: cpp07 main + array

--- cpp07/ex02/incl/Array.hpp
+++ cpp07/ex02/incl/Array.hpp
@@ -16,15 +16,21 @@ public:
 	Array& operator=(const Array& other);
 	Array(const Array& other);
 	T& operator[](unsigned int index);
-	unsigned int size();
+	const T& operator[](unsigned int index) const;
+	unsigned int size() const;
 };
 
 template <typename T> Array<T>::Array():_array(NULL), _len(0)
 {
 }
-template <typename T> Array<T>::Array(unsigned int len): _len(len)
+template <typename T> Array<T>::Array(unsigned int len):_array(NULL), _len(len)
 {
-	_array = new T[_len];
+	try{
+	 _array = new T[_len]; 
+	}
+	catch (const std::exception& e){
+		 std::cerr << e.what() << '\n';
+	}
 }
 
 template <typename T> Array<T>::~Array()
@@ -34,20 +40,29 @@ template <typename T> Array<T>::~Array()
 
 template <typename T>
 Array<T>::Array(const Array& other){
+	_len = other._len;
 	_array = new T[_len];
 	for (unsigned int i = 0; i <other._len; i++)
 		_array[i] = other._array[i];
-	_len = other._len;
 
 }
 template <typename T> Array<T> & Array<T>::operator=(const Array& other){
-	if (this != other){
+	if (this != &other){
 		delete [] _array;
+		_len = other._len;
 		_array = new T[_len];
 		for (unsigned int i = 0; i <other._len; i++)
 			_array[i] = other._array[i];
-		_len = other._len;
 	}
+	return *this;
+
+}
+template <typename T> const T& Array<T>::operator[](unsigned int index) const{
+	if (index < _len)
+	{
+		return _array[index];
+	}
+	throw std::out_of_range("out of range\n");
 
 }
 
@@ -56,11 +71,10 @@ template <typename T> T& Array<T>::operator[](unsigned int index){
 	{
 		return _array[index];
 	}
-	else
-		throw std::out_of_range("out of range\n");
+	throw std::out_of_range("out of range\n");
 
 }
-template <typename T> unsigned int Array<T>:: size(){
+template <typename T> unsigned int Array<T>:: size() const{
 
 	return _len;
 }
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -2,6 +2,7 @@
 #include <Array.hpp>
 
 #define MAX_VAL 10
+/*
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
@@ -9,7 +10,7 @@ int main(int, char**)
     srand(time(NULL));
     for (int i = 0; i < MAX_VAL; i++)
     {
-        const int value = i;
+        const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
@@ -37,7 +38,58 @@ int main(int, char**)
     }
     try
     {
-        numbers[MAX_VAL] = MAX_VAL;
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
+*/
+int main(int, char**)
+{
+    Array<std::string> numbers(MAX_VAL);
+    std::string* mirror = new std::string[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        std::string value = std::to_string(i+1);
+        numbers[i] = value;
+        std::cout << numbers[i] << std::endl;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<std::string> tmp = numbers;
+        Array<std::string> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = '0';
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = '0';
     }
     catch(const std::exception& e)
     {
