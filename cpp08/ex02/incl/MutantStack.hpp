e05a88820432d331b338498d0f23bae8a2624553
Jenni Korvenpaa <jkorvenp@student.hive.fi>
2026-06-10 14:37:09 +0300

    feat: more tests to mains

--- cpp08/ex02/incl/MutantStack.hpp
+++ cpp08/ex02/incl/MutantStack.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stack>
 
+
 template <typename T> 
 class MutantStack : public std::stack<T>{
 
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,4 +1,5 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -11,7 +12,6 @@ int main()
 		mstack.push(3);
 		mstack.push(5);
 		mstack.push(737);
-		//[...]
 		mstack.push(0);
 		MutantStack<int>::iterator it = mstack.begin();
 		MutantStack<int>::iterator ite = mstack.end();
@@ -25,3 +25,28 @@ int main()
 		std::stack<int> s(mstack);
 		return 0;
 }
+
+// int main()
+// {
+//     std::list<int> lst;
+//     lst.push_back(5);
+//     lst.push_back(17);
+//     std::cout << lst.back() << std::endl;
+//     lst.pop_back();
+//     std::cout << lst.size() << std::endl;
+//     lst.push_back(3);
+//     lst.push_back(5);
+//     lst.push_back(737);
+//     lst.push_back(0);
+//     std::list<int>::iterator it = lst.begin();
+//     std::list<int>::iterator ite = lst.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::list<int> copy(lst);
+//     return 0;
+// }
