45fa6cba91ea939e5d73a14eb6d73e7fa6c02250
Jenni Korvenpää <korvenpaajenni@gmail.com>
2026-07-17 13:41:03 +0300

    feat class PmergeMe, main

--- cpp09/ex02/incl/PmergeMe.hpp
+++ cpp09/ex02/incl/PmergeMe.hpp
@@ -1,1 +1,18 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <ctime> // cpu time std::clock_t start = std::clock;
+//double us = (end - start) * 1000000.0 / CLOCKS_PER_SEC;
+
+class PmergeMe{
+	private:
+	std::vector<int>	_vec;
+	std::deque<int>		_deq;
+	public:
+	PmergeMe(){};
+	~PmergeMe(){};
+	PmergeMe(const PmergeMe &other);
+	PmergeMe&	operator=(const PmergeMe &other);
+	void parseInput(int ac, std::string av);
+};
--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -1,1 +1,15 @@
 #include "PmergeMe.hpp"
+
+int	main (int argc, char **argv){
+	if (argc < 2){
+		std::cerr << "Error: not enough arguments\n";
+		return 1;
+	}
+	try{
+		parseInput(argc, argv);
+	}
+	catch (std::exception &e){
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
